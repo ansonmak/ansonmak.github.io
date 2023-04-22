ec487157fc6a5dc414595588cbac986dccbc7788
ansonmak <ansonmak1997@gmail.com>
2023-04-22 10:53:01 +0100

    Change Section title

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@
 				parallax: true,
 
 			// Parallax factor (lower = more intense, higher = less intense).
-				parallaxFactor: 1.7,
+				parallaxFactor: 1,
 
 			// Background offset from top
 				topOffset: -170
